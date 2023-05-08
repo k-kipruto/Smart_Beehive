8deb3debed4e563bfd765dc942d57bea6bdd19d7
kipruto <kuruibonny@gmail.com>
2023-05-08 20:19:50 +0300

    fixed download link archive type

--- docs/gcc-arm.sh
+++ docs/gcc-arm.sh
@@ -4,8 +4,10 @@
 
 VER=${VER:-'10.3-2021.10'}
 
+    # https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
 URL=https://developer.arm.com/-/media/Files/downloads/gnu-rm/${VER}/gcc-arm-none-eabi-${VER}-x86_64-linux.tar.bz2
 # URL=https://developer.arm.com/-/media/Files/downloads/gnu/${VER}/binrel/gcc-arm-${VER}-x86_64-arm-none-eabi.tar.xz
+
 
 echo "Creating gcc-arm-none-eabi x86_64 debian package" 
 echo "version: $VER"
